2e110a340e2d9a5c777da444c1e43fffd3706378
drizlye0 <danielkastillo03@gmail.com>
2025-10-17 20:42:03 -0700

    clean project

--- src/Components/Camera/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Camera() {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
-}
--- src/Screens/Home/Components/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Stats } from "./Stats";
-export { default as History } from "./History";
-export { default as Overview } from "./Overview";
